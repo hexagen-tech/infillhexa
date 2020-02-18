26dbc98c6b0c537422cac97080f5cf400fa0b6c1
skrdas9995 <skrdas9995@email.com>
2020-02-18 16:58:57 +0530

    second commit

--- model/user.js
+++ model/user.js
@@ -1,4 +1,6 @@
-var Url = 'mongodb://localhost:27017/demo';
+// var Url = 'mongodb://localhost:27017/demo';
+var Url = 'mongodb+srv://priyanka:<password>@flashmob-test-3adtm.mongodb.net/test?retryWrites=true&w=majority';
+
 const mongoose = require('mongoose');
 mongoose.connect(Url, {useNewUrlParser: true, useCreateIndex: true,  useUnifiedTopology: true });
 var conn =mongoose.Collection;
